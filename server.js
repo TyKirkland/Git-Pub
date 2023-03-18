6602755dc0b34f8c58ddfa9f4ee1ce3a3e59be9f
Ty <Ty@Tys-Macbook.local>
2023-03-18 15:21:05 -0400

    default route

---
+++ server.js
@@ -0,0 +1,12 @@
+const express = require('express');
+
+const app = express();
+
+app.get('/', (req, res) => {
+    res.send('Welcome to the Gitpub App!');
+})
+
+//keep this at the bottom
+app.listen(3000, () => {
+    console.log('Listening on port 3000!');
+})
